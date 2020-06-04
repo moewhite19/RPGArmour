d2a4714c8d4a8cbe6347f305b6c6830374de501b
MoeWhite19 <MoeWhite19@163.com>
2020-06-04 08:35:16 +0800

    现在武士刀放在左手攻击时如果右手有物品的话会直接掉落

--- src/cn/whiteg/rpgArmour/custItems/SamuraiSword.java
+++ src/cn/whiteg/rpgArmour/custItems/SamuraiSword.java
@@ -6,6 +6,7 @@ import cn.whiteg.rpgArmour.api.CustItem_CustModle;
 import cn.whiteg.rpgArmour.event.PlayerAttackMissEvent;
 import cn.whiteg.rpgArmour.utils.ItemToolUtil;
 import cn.whiteg.rpgArmour.utils.RandomUtil;
+import cn.whiteg.rpgArmour.utils.VectorUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -84,151 +85,79 @@ public class SamuraiSword extends CustItem_CustModle implements Listener {
         }
     }
 
-    //
-//    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
-//    public void onInvClick(InventoryClickEvent event) {
-//        ItemStack item = event.getCurrentItem();
-//        if (item != null && item.getType() == getMaterial() && ItemSwitch(item,id2,getId())){
-//            Location loc = event.getWhoClicked().getLocation();
-//            loc.getWorld().playSound(loc,sound_put,1,1);
-//        }
-//    }
-
-    @EventHandler(priority = EventPriority.HIGH)
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
     public void onEntityDamage(EntityDamageByEntityEvent event) {
-        if (event.getDamage() < 0.5 || !(event.getDamager() instanceof LivingEntity && (event.getEntity() instanceof Mob || event.getEntity() instanceof HumanEntity) && event.getCause() == EntityDamageEvent.DamageCause.ENTITY_ATTACK))
+        if (event.getDamage() < 0.5 || !(event.getDamager() instanceof LivingEntity && (event.getEntity() instanceof Mob || event.getEntity() instanceof HumanEntity) && event.getCause() == EntityDamageEvent.DamageCause.ENTITY_ATTACK)){
             return;
-//        if (!(event.getDamager() instanceof LivingEntity) || !(event.getEntity() instanceof Creature) || event.getCause() != EntityDamageEvent.DamageCause.ENTITY_ATTACK)
-        final LivingEntity livingEntity = (LivingEntity) event.getDamager();
-        if (event.isCancelled()) return;
-        final EntityEquipment pi = livingEntity.getEquipment();
-        ItemStack item = pi.getItemInMainHand();
-        //使用技能.玩家必须空着主手 ， 其他实体直接覆盖主手w
-        if (isAir(item) || !(event.getDamager() instanceof Player)){
-            item = pi.getItemInOffHand();
-            if (isItem(item) == getId()){
-                if (hasCd(livingEntity)){
-//                    event.setCancelled(true);
-                    return;
-                }
-                Location loc = livingEntity.getLocation();
-                event.setDamage(event.getDamage() + skillDamage);
-                if (ItemToolUtil.damage(item,3)){
-                    loc = livingEntity.getLocation();
-                    loc.getWorld().playSound(loc,sound_damagebad,1,1);
-                    pi.setItemInOffHand(null);
-                    return;
-                }
-                ItemMeta im = item.getItemMeta();
-                im.setCustomModelData(id2);
-                item.setItemMeta(im);
-                pi.setItemInMainHand(item);
-                loc.getWorld().playSound(loc,sound_unsheathed2,1,1);
-                pi.setItemInOffHand(scabbard.createItem());
-                setCd(livingEntity,25);
-                Bukkit.getScheduler().runTaskLater(RPGArmour.plugin,() -> {
-                    final ItemStack i = pi.getItemInMainHand();
-                    if (scabbard.is(pi.getItemInOffHand()) && isItem(i) == id2){
-                        ItemMeta m = i.getItemMeta();
-                        m.setCustomModelData(getId());
-                        i.setItemMeta(m);
-                        pi.setItemInOffHand(i);
-                        pi.setItemInMainHand(null);
-                        Location l = pi.getHolder().getLocation();
-                        l.getWorld().playSound(l,sound_put,1,1);
-                    }
-                    if (is(i)){
-                    }
-                },15);
+        }
+        final LivingEntity damager = (LivingEntity) event.getDamager();
+        final EntityEquipment equipment = damager.getEquipment();
+//        if (pi == null) return;
+        ItemStack main = equipment.getItemInMainHand();
+        if (isItem(main) == id2){
+            if (hasCd(damager)) return;
+            Location loc = event.getEntity().getLocation();
+            loc.getWorld().playSound(loc,sound_att,1,1);
+            setCd(damager,15);
+            double damage = event.getDamage() + this.damage;
+            if (damager instanceof Mob) damage += 5;
+            event.setDamage(damage);
+            if (ItemToolUtil.damage(main,1)){
+                loc = damager.getLocation();
+                loc.getWorld().playSound(loc,sound_damagebad,1,1);
+                equipment.setItemInMainHand(null);
             }
             return;
         }
-        if (isItem(item) == id2){
-            if (hasCd(livingEntity)) return;
-            Location loc = event.getEntity().getLocation();
-            loc.getWorld().playSound(loc,sound_att,1,1);
-            setCd(livingEntity,15);
-            double damage = event.getDamage() + this.damage;
-            if (livingEntity instanceof Mob) damage += 5;
-            event.setDamage(damage);
-            if (ItemToolUtil.damage(item,1)){
-                loc = livingEntity.getLocation();
-                loc.getWorld().playSound(loc,sound_damagebad,1,1);
-                pi.setItemInMainHand(null);
+
+        ItemStack off = equipment.getItemInOffHand();
+        if (isItem(off) == getId()){
+            if (hasCd(damager)){
+                return;
             }
+            Location loc = damager.getLocation();
+
+            //使用技能时如果主手有物品则掉落
+            if (!isAir(main)){
+//                if (damager instanceof Player){
+//                    return;
+//                }
+                Item dropItem = loc.getWorld().dropItem(loc,main);
+                if (dropItem.isDead()) return;
+                dropItem.setVelocity(VectorUtils.viewVector(loc));
+            }
+            event.setDamage(event.getDamage() + skillDamage);
+            if (ItemToolUtil.damage(off,3)){
+                loc = damager.getLocation();
+                loc.getWorld().playSound(loc,sound_damagebad,1,1);
+                equipment.setItemInOffHand(null);
+                return;
+            }
+            ItemMeta im = off.getItemMeta();
+            im.setCustomModelData(id2);
+            off.setItemMeta(im);
+            equipment.setItemInMainHand(off);
+            loc.getWorld().playSound(loc,sound_unsheathed2,1,1);
+            equipment.setItemInOffHand(scabbard.createItem());
+            setCd(damager,25);
+            Bukkit.getScheduler().runTaskLater(RPGArmour.plugin,() -> {
+                final ItemStack i = equipment.getItemInMainHand();
+                if (scabbard.is(equipment.getItemInOffHand()) && isItem(i) == id2){
+                    ItemMeta m = i.getItemMeta();
+                    m.setCustomModelData(getId());
+                    i.setItemMeta(m);
+                    equipment.setItemInOffHand(i);
+                    equipment.setItemInMainHand(null);
+                    Location l = equipment.getHolder().getLocation();
+                    l.getWorld().playSound(l,sound_put,1,1);
+                }
+                if (is(i)){
+                }
+            },15);
             return;
         }
     }
 
-/*    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-    private void onItemr(PlayerrItemEvent event) {
-        ItemStack item = event.getItemDrop().getItemStack();
-        if (item.getType() == getMaterial() && ItemSwitch(item,id2,getId())){
-            Location loc = event.getPlayer().getLocation();
-            loc.getWorld().playSound(loc,sound_put,1,1);
-            event.setCancelled(true);
-        }
-    }*/
-
-//    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
-//    public void onHat(ArmourChangeEvent event) {
-//        if (event.getType() != ArmourChangeEvent.ArmourType.HELMET || !event.isWear()) return;
-//        ItemStack item = event.getItem();
-//        if (is(item)){
-//            if (ItemSwitch(item,getId(),id2)){
-//                Location loc = event.getPlayer().getLocation();
-//                loc.getWorld().playSound(loc,sound_unsheathed,1,1);
-//            }
-//        }
-//    }
-
-    /*
-        @EventHandler(priority = EventPriority.HIGH)
-        public void onUse(PlayerInteractEvent event) {
-            if (event.getAction() != Action.RIGHT_CLICK_AIR) return;
-            if (event.getHand() == EquipmentSlot.HAND){
-                ItemStack item = event.getItem();
-                if (this.is(item)){
-                    ItemMeta im = item.getItemMeta();
-                    int i = im.getCustomModelData();
-                    if (i == getId()){
-                        if (event.getPlayer().hasCooldown(getMaterial())) return;
-                        im.setCustomModelData(id2);
-                        item.setItemMeta(im);
-                        Location loc = event.getPlayer().getLocation();
-                        loc.getWorld().playSound(loc,sound_unsheathed,1,1);
-                    } else {
-                        im.setCustomModelData(getId());
-                        item.setItemMeta(im);
-                        Location loc = event.getPlayer().getLocation();
-                        loc.getWorld().playSound(loc,sound_put,1,1);
-                        setCd(event.getPlayer(),10);
-                    }
-                }
-            } else if (event.getHand() == EquipmentSlot.OFF_HAND){
-                ItemStack item = event.getItem();
-                if (this.is(item)){
-                    PlayerInventory pi = event.getPlayer().getInventory();
-                    if (pi.getHolder().getGameMode() != GameMode.SURVIVAL) return;
-                    ItemStack hand = pi.getItemInMainHand();
-                    if (isAir(hand)){
-                        if (event.getPlayer().hasCooldown(getMaterial())) return;
-                        ItemMeta im = item.getItemMeta();
-                        int i = im.getCustomModelData();
-                        if (i == getId()){
-                            im.setCustomModelData(id2);
-                            item.setItemMeta(im);
-                            Location loc = event.getPlayer().getLocation();
-                            loc.getWorld().playSound(loc,sound_unsheathed,1,1);
-                            pi.setItemInOffHand(null);
-                            pi.setItemInMainHand(item);
-                        }
-                    }
-
-                }
-            }
-        }
-    */
     @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
     public void onInvClick(InventoryClickEvent event) {
         if (event.getClick() != ClickType.RIGHT) return;
@@ -330,20 +259,6 @@ public class SamuraiSword extends CustItem_CustModle implements Listener {
 //        }
     }
 
-/*
-    @EventHandler(ignoreCancelled = true)
-    public void onItemHeld(PlayerItemHeldEvent event) {
-        PlayerInventory pi = event.getPlayer().getInventory();
-        ItemStack main = pi.getItemInMainHand();
-        if (is(main)){
-            if (ItemSwitch(main,id2,getId())){
-                Location loc = event.getPlayer().getLocation();
-                loc.getWorld().playSound(loc,sound_put,1,1);
-            }
-        }
-    }
-*/
-
     private boolean isAir(ItemStack item) {
         return item == null || item.getType() == Material.AIR;
     }
@@ -365,24 +280,6 @@ public class SamuraiSword extends CustItem_CustModle implements Listener {
         }
         return false;
     }
-//    private boolean hasCd(Entity player) {
-//        if (cdMap.containsKey(player.getName())){
-//            long now = System.currentTimeMillis();
-//            return cdMap.get(player.getName()) < now;
-//        }
-//        return true;
-//    }
-//
-//    @SuppressWarnings("all")
-//    private void setCd(Entity player) {
-//        cdMap.put(new String(player.getName()),System.currentTimeMillis() + 500);
-//    }
-
-//    private double attEntity(Entity atter,Entity atted,ItemStack item) {
-//        if (!hasCd(atter)) return 0;
-//        setCd(atter);
-//        return 12;
-//    }
 
 
     @SuppressWarnings("all")
@@ -424,7 +321,8 @@ public class SamuraiSword extends CustItem_CustModle implements Listener {
         if (spawnChance <= 0) return;
         if (!(event.getEntity() instanceof LivingEntity)) return;
         LivingEntity entity = (LivingEntity) event.getEntity();
-        if(entity.fromMobSpawner() || entity.getEntitySpawnReason() == CreatureSpawnEvent.SpawnReason.SPAWNER_EGG) return;
+        if (entity.fromMobSpawner() || entity.getEntitySpawnReason() == CreatureSpawnEvent.SpawnReason.SPAWNER_EGG)
+            return;
         EntityEquipment ej = entity.getEquipment();
         Random random = RandomUtil.getRandom();
         if (ej == null) return;
@@ -448,7 +346,7 @@ public class SamuraiSword extends CustItem_CustModle implements Listener {
         }
     }
 
-    class Scabbard extends CustItem_CustModle {
+    static class Scabbard extends CustItem_CustModle {
         public Scabbard() {
             super(Material.BOWL,38,"§9刀鞘");
         }
