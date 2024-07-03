0116ff97c1d85eb0b9fa845aa32bf30728f4004b
MoeWhite19 <MoeWhite19@163.com>
2024-07-03 17:03:25 +0800

    尝试使用Paper内建的PlayerJumpEvent

--- src/cn/whiteg/rpgArmour/event/PlayerJumpEvent.java
+++ src/cn/whiteg/rpgArmour/event/PlayerJumpEvent.java
@@ -14,18 +14,33 @@ public class PlayerJumpEvent extends PlayerEvent implements Cancellable {
     private static final HandlerList handlerList = new HandlerList();
 
     static {
-        Bukkit.getScheduler().runTask(RPGArmour.plugin,() -> {
-            RPGArmour.plugin.regListener(new Listener() {
-                @EventHandler(ignoreCancelled = true)
-                public void onMove(PlayerMoveEvent event) {
-                    final double y = event.getTo().getY() - event.getFrom().getY();
-                    //todo 有跳跃增强buff无法触发，可能是版本更新时跳跃增强属性被改了
-                    if (y != 0.41999998688697815D && y != 0.5199999809265137 && y != 0.6200000047683716D)
-                        return;
-                    PlayerJumpEvent pj = new PlayerJumpEvent(event.getPlayer());
-                    pj.call();
-                }
-            });
+        final RPGArmour plugin = RPGArmour.plugin;
+        Bukkit.getScheduler().runTask(plugin,() -> {
+
+            try{
+                plugin.regListener(new Listener() {
+                    @EventHandler(ignoreCancelled = true)
+                    public void onJump(com.destroystokyo.paper.event.player.PlayerJumpEvent event) {
+                        PlayerJumpEvent e = new PlayerJumpEvent(event.getPlayer());
+                        e.call();
+                        event.setCancelled(e.isCancelled());
+                    }
+                });
+            }catch (Throwable e){
+                plugin.regListener(new Listener() {
+                    @EventHandler(ignoreCancelled = true)
+                    public void onMove(PlayerMoveEvent event) {
+                        final double y = event.getTo().getY() - event.getFrom().getY();
+                        //todo 有跳跃增强buff无法触发，可能是版本更新时跳跃增强属性被改了
+                        if (y != 0.41999998688697815D && y != 0.5199999809265137 && y != 0.6200000047683716D) return;
+                        PlayerJumpEvent e = new PlayerJumpEvent(event.getPlayer());
+                        e.call();
+                        event.setCancelled(e.isCancelled());
+                    }
+                });
+            }
+
+            ;
         });
     }
 
